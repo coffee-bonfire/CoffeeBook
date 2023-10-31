d891ba9b6103f827cb9654cf6af70b29c5c899df
coffee-bonfire <coffeelike.ncnn@gmail.com>
2023-10-31 22:07:24 +0900

    :+1: hitl applicationのとエントリーポイントの作成

---
+++ app/src/main/java/com/example/coffeebooksapp/App.kt
@@ -0,0 +1,9 @@
+package com.example.coffeebooksapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App: Application() {
+
+}
--- app/src/main/java/com/example/coffeebooksapp/MainActivity.kt
+++ app/src/main/java/com/example/coffeebooksapp/MainActivity.kt
@@ -1,17 +1,27 @@
 package com.example.coffeebooksapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.coffeebooksapp.components.EditDialog
 import com.example.coffeebooksapp.ui.theme.CoffeeBooksAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,25 +32,23 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainContent()
                 }
             }
         }
     }
 }
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CoffeeBooksAppTheme {
-        Greeting("Android")
+fun MainContent(){
+    EditDialog()
+    Scaffold(floatingActionButton = {
+        FloatingActionButton(onClick = { /*TODO*/ }) {
+            Icon(imageVector = Icons.Default.Add, contentDescription = "図鑑新規作成" )
+        }
+    }) {
+        
     }
 }

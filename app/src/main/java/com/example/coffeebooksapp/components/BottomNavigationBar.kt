182c7b09c3479f5c6fc90fb857d07345930277d1
coffee-bonfire <coffeelike.ncnn@gmail.com>
2023-11-25 17:10:31 +0900

    :green_heart: リファクタリング

---
+++ app/src/main/java/com/example/coffeebooksapp/components/BottomNavigationBar.kt
@@ -0,0 +1,103 @@
+package com.example.coffeebooksapp.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+
+@Composable
+fun BottomNavigationBar(navController: NavController) {
+    val items = listOf(
+        NavigationItem.Home,
+        NavigationItem.Setting,
+    )
+    var selectedItem by remember { mutableStateOf(0) }
+    var currentRoute by remember { mutableStateOf(NavigationItem.Home.route) }
+
+    items.forEachIndexed { index, navigationItem ->
+        if (navigationItem.route == currentRoute) {
+            selectedItem = index
+        }
+    }
+
+    NavigationBar {
+        items.forEachIndexed { index, item ->
+            NavigationBarItem(
+                alwaysShowLabel = true,
+                icon = { Icon(item.icon!!, contentDescription = item.title) },
+                label = { Text(item.title) },
+                selected = selectedItem == index,
+                onClick = {
+                    selectedItem = index
+                    currentRoute = item.route
+                    navController.navigate(item.route) {
+                        navController.graph.startDestinationRoute?.let { route ->
+                            popUpTo(route) {
+                                saveState = true
+                            }
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                }
+            )
+        }
+    }
+}
+
+sealed class NavigationItem(var route: String, val icon: ImageVector?, var title: String) {
+    object Home : NavigationItem("Home", Icons.Default.Home, "Home")
+    object Setting : NavigationItem("History", Icons.Default.Settings, "Setting")
+}
+
+@Composable
+fun Navigations(navController: NavHostController) {
+    NavHost(navController, startDestination = NavigationItem.Home.route) {
+        composable(NavigationItem.Home.route) {
+            HomeScreen()
+        }
+        composable(NavigationItem.Setting.route) {
+            SettingScreen()
+        }
+    }
+}
+
+@Composable
+fun SettingScreen(){
+    CenterText(text = "Setting")
+}
+
+@Composable
+fun CenterText(text: String) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = text, fontSize = 32.sp)
+    }
+}
+
+@Composable
+fun HomeScreen() {
+    CenterText(text = "Home")
+}

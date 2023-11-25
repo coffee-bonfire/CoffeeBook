1374438650b659ade26c012d686e6c962f06ba2f
coffee-bonfire <coffeelike.ncnn@gmail.com>
2023-11-25 12:36:29 +0900

    :+1: ナビゲーションの追加

--- app/src/main/java/com/example/coffeebooksapp/MainActivity.kt
+++ app/src/main/java/com/example/coffeebooksapp/MainActivity.kt
@@ -4,20 +4,44 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.coffeebooksapp.components.EditDialog
 import com.example.coffeebooksapp.ui.theme.CoffeeBooksAppTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -27,13 +51,14 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val navController = rememberNavController()
             CoffeeBooksAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MainContent()
+                    MainContent(navController = navController)
                 }
             }
         }
@@ -43,7 +68,9 @@ class MainActivity : ComponentActivity() {
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MainContent(){
+fun MainContent(
+    navController: NavHostController
+){
     val isShowDialog = remember {
       mutableStateOf(false)
     }
@@ -53,7 +80,23 @@ fun MainContent(){
         EditDialog(isShowDialog,context)
     }
 
-    Scaffold(floatingActionButton = {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        textAlign = TextAlign.Center,
+                        text = "コーヒー図鑑"
+                    )
+                }
+            )
+        },
+        bottomBar = {
+            BottomAppBar(modifier = Modifier) {
+                BottomNavigationBar(navController = navController)
+            }
+        },
+        floatingActionButton = {
         FloatingActionButton(
             onClick = {
                 isShowDialog.value = true
@@ -61,7 +104,96 @@ fun MainContent(){
         {
             Icon(imageVector = Icons.Default.Add, contentDescription = "図鑑新規作成" )
         }
-    }) {
-        
+    }) { innerPadding ->
+        Box(
+            modifier = Modifier.padding(
+                PaddingValues(
+                    0.dp,
+                    0.dp,
+                    0.dp,
+                    innerPadding.calculateBottomPadding()
+                )
+            )
+        ) {
+            Navigations(navController = navController)
+        }
     }
+}
+
+@Composable
+fun BottomNavigationBar(navController: NavController) {
+    val items = listOf(
+        NavigationItem.Home,
+        NavigationItem.Setting,
+    )
+    var selectedItem by remember { mutableStateOf(0) }
+    var currentRoute by remember { mutableStateOf(NavigationItem.Home.route) }
+
+    items.forEachIndexed { index, navigationItem ->
+        if (navigationItem.route == currentRoute) {
+            selectedItem = index
+        }
+    }
+
+    NavigationBar {
+        items.forEachIndexed { index, item ->
+            NavigationBarItem(
+                alwaysShowLabel = true,
+                icon = { Icon(item.icon!!, contentDescription = item.title) },
+                label = { Text(item.title) },
+                selected = selectedItem == index,
+                onClick = {
+                    selectedItem = index
+                    currentRoute = item.route
+                    navController.navigate(item.route) {
+                        navController.graph.startDestinationRoute?.let { route ->
+                            popUpTo(route) {
+                                saveState = true
+                            }
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                }
+            )
+        }
+    }
+}
+
+sealed class NavigationItem(var route: String, val icon: ImageVector?, var title: String) {
+    object Home : NavigationItem("Home", Icons.Default.Home, "Home")
+    object Setting : NavigationItem("History", Icons.Default.Settings, "Setting")
+}
+
+@Composable
+fun Navigations(navController: NavHostController) {
+    NavHost(navController, startDestination = NavigationItem.Home.route) {
+        composable(NavigationItem.Home.route) {
+            HomeScreen()
+        }
+        composable(NavigationItem.Setting.route) {
+            SettingScreen()
+        }
+    }
+}
+
+@Composable
+fun SettingScreen(){
+    CenterText(text = "Setting")
+}
+
+@Composable
+fun CenterText(text: String) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = text, fontSize = 32.sp)
+    }
+}
+
+@Composable
+fun HomeScreen() {
+    CenterText(text = "Home")
 }

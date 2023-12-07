16e944fc9bcf5cef286bde7e2868520d21ab469a
coffee-bonfire <coffeelike.ncnn@gmail.com>
2023-12-07 23:23:11 +0900

    :green_heart: リファクタリング

--- app/src/main/java/com/example/coffeebooksapp/components/SettingComponent.kt
+++ app/src/main/java/com/example/coffeebooksapp/components/SettingComponent.kt
@@ -4,14 +4,11 @@ import android.content.Context
 import android.content.Intent
 import android.util.Log
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -23,7 +20,6 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SettingComponent() {
     val context = LocalContext.current

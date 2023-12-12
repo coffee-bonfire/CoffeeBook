fea9a4a4dc79928b4f869b81b818392adc68b189
coffee-bonfire <coffeelike.ncnn@gmail.com>
2023-12-12 23:28:00 +0900

    :+1: レイアウト微修正

--- app/src/main/java/com/example/coffeebooksapp/components/SettingComponent.kt
+++ app/src/main/java/com/example/coffeebooksapp/components/SettingComponent.kt
@@ -3,22 +3,20 @@ package com.example.coffeebooksapp.components
 import android.content.Context
 import android.content.Intent
 import android.util.Log
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -27,26 +25,26 @@ import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 @Composable
 fun SettingComponent() {
     val context = LocalContext.current
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(100.dp)
-            .background(Color.White.copy(alpha = 0.3f))
-            .padding(horizontal = 10.dp, vertical = 20.dp).clickable {
-                startOssLicensesMenuActivity(context)
-            }.border(
-                width = 2.dp,
-                color = Color.DarkGray,
-            )
-    ) {
-        Text(
-            modifier = Modifier.padding(20.dp),
-            text = "バージョン情報",
-            fontSize = 14.sp,
-            fontWeight = FontWeight.Bold,
-            textAlign = TextAlign.Center,
-        )
+    Column {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 10.dp, vertical = 20.dp)
+                .clickable {
+                    startOssLicensesMenuActivity(context)
+                }
+        ) {
+            Text(
 
+                text = "バージョン情報",
+                fontSize = 20.sp,
+                textAlign = TextAlign.Left,
+            )
+            Spacer(modifier = Modifier.width(10.dp))
+            Icon(imageVector = Icons.Default.KeyboardArrowRight, contentDescription = "version_info" )
+            Spacer(modifier = Modifier.width(30.dp))
+
+        }
     }
 }
 

d72772b8f6c86d1329df7b7c76480374a3f2d407
coffee-bonfire <coffeelike.ncnn@gmail.com>
2024-07-17 23:15:34 +0900

    :+1: 図鑑アイテム詳細画面

--- app/src/main/java/com/dandanbiyori/coffeebooksapp/components/BookItemDetailView.kt
+++ app/src/main/java/com/dandanbiyori/coffeebooksapp/components/BookItemDetailView.kt
@@ -4,20 +4,29 @@ package com.dandanbiyori.coffeebooksapp.components
 import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
 import android.net.Uri
-import androidx.annotation.Nullable
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -26,13 +35,13 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.core.graphics.drawable.toBitmap
-import androidx.lifecycle.LiveData
 import com.dandanbiyori.coffeebooksapp.BookItem
 import com.dandanbiyori.coffeebooksapp.R
 
@@ -44,13 +53,56 @@ fun BookItemDetailView(
     bookItem: BookItem?
 ) {
     val scrollState = rememberScrollState()
-    if (bookItem != null) {
-        BookItemDetailContent(
-            bookItem,
-            scrollState
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ){
+        if (bookItem != null) {
+            BookItemDetailContent(
+                bookItem,
+                scrollState
+            )
+        }
+        BookItemDetailTopBar(
+            onClickBack,
         )
     }
 
+}
+
+@Composable
+fun BookItemDetailTopBar(
+    onClickBack: () -> Unit,
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxSize()
+            .systemBarsPadding()
+            .padding(top = Dimens.ToolbarIconPadding),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        val iconModifier = Modifier
+            .sizeIn(
+                maxWidth = Dimens.ToolbarIconSize,
+                maxHeight = Dimens.ToolbarIconSize
+            )
+            .background(
+                color = MaterialTheme.colorScheme.surface,
+                shape = CircleShape
+            )
+
+        IconButton(
+            onClick = onClickBack,
+            modifier = Modifier
+                .padding(start = Dimens.ToolbarIconPadding)
+                .then(iconModifier)
+        ) {
+            Icon(
+                Icons.Filled.ArrowBack,
+                contentDescription = "アイコン",
+                tint = MaterialTheme.colorScheme.onSurface
+            )
+        }
+    }
 }
 
 @Composable
@@ -72,6 +124,7 @@ fun BookItemDetailContent(
                     color = Color.Gray,
                     fontSize = 30.sp,
                     fontWeight = FontWeight.ExtraBold,
+                    modifier = Modifier.padding(20.dp, 0.dp)
                 )
                 Spacer(modifier = Modifier.height(10.dp))
                 Column(
@@ -103,7 +156,7 @@ private fun BookItemImage(
     val uri: Uri? = bookItem?.let { Util.convertStringToUri(it.imageUri) }
 
     // uriをBitmapに変換
-    if (uri != null) {
+    if (uri != null && uri.toString().isNotEmpty()) {
         imageBitmap = Util.convertUriToBitmap(uri, context)
     }
 

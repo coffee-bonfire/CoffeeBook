bc9e5f8b8b9c13000ead405aa101cde801b5e7f8
coffee-bonfire <coffeelike.ncnn@gmail.com>
2024-10-23 21:57:11 +0900

    :+1: gradle fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -116,8 +116,4 @@ dependencies {
     // Import the Firebase BoM
     implementation(platform("com.google.firebase:firebase-bom:32.7.1"))
 
-    // When using the BoM, don't specify versions in Firebase dependencies
-    implementation("com.google.firebase:firebase-analytics")
-    implementation("com.google.firebase:firebase-storage")
-
 }

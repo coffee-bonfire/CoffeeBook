fdb83dac08a58a8463c8a6f2b235fa3a2c4682e3
coffee-bonfire <coffeelike.ncnn@gmail.com>
2024-12-16 23:55:14 +0900

    :+1: database 3

--- app/src/main/java/com/dandanbiyori/coffeebooksapp/AppDatabase.kt
+++ app/src/main/java/com/dandanbiyori/coffeebooksapp/AppDatabase.kt
@@ -3,7 +3,7 @@ package com.dandanbiyori.coffeebooksapp
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [Book::class, BookItem::class], version = 1, exportSchema = false)
+@Database(entities = [Book::class, BookItem::class], version = 3, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun bookDao(): BookDao
     abstract fun bookItemDao(): BookItemDao
